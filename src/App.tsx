5bf2d758ae09e394c2aa57c50feb01b2f2525ae1
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-01-23 12:55:56 +0530

    forms,modals,cards,tables explored

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,21 @@
-import { Card, Col, Divider, Layout, Menu, Row, Space, Typography } from "antd";
+import {
+  Button,
+  Card,
+  Col,
+  Divider,
+  Form,
+  Input,
+  Layout,
+  Menu,
+  Row,
+  Space,
+  Table,
+  Tag,
+  Typography,
+  TimePicker,
+  Empty,
+  Modal,
+} from "antd";
 import { Content, Header } from "antd/es/layout/layout";
 import Sider from "antd/es/layout/Sider";
 import { useState } from "react";
@@ -8,10 +25,27 @@ import { GrOrganization } from "react-icons/gr";
 import { BsPerson } from "react-icons/bs";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineMoneyCollect } from "react-icons/ai";
-import { Anchor } from "antd";
+// import { Anchor } from "antd";
+import { faker } from "@faker-js/faker";
+import ButtonGroup from "antd/es/button/button-group";
+import dayjs from "dayjs";
 
+const generateData = () => {
+  const dat = [];
+  for (let i = 0; i < 34; i++) {
+    dat.push({
+      id: faker.datatype.number(1000),
+      name: faker.name.fullName(),
+      email: faker.internet.email(),
+      status: Math.random() > 0.5 ? true : false,
+    });
+  }
+  return dat;
+};
+const data = generateData();
 function App() {
   const [collapsed, setCollapsed] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <>
       <Layout className="container">
@@ -88,13 +122,58 @@ function App() {
               <Card>
                 <Space direction="horizontal">
                   <AiOutlineMoneyCollect />
-
                   <small>Total sales</small>
                 </Space>
                 <Typography.Title>$23456</Typography.Title>
               </Card>
             </Space>
             <Divider />
+            <Card>
+              <Button
+                type="primary"
+                onClick={() => setModalOpen(true)}
+                // onClick={() => {
+                //   Modal.confirm({
+                //     title: "Do you want to delete these items?",
+                //     content:
+                //       "When clicked the OK button, this dialog will be closed after 1 second",
+                //   });
+                // }}
+              >
+                Open form
+              </Button>
+              <Empty />
+              <Modal open={modalOpen} onCancel={() => setModalOpen(false)}>
+                <Form
+                  onFinish={(values) => {
+                    console.log(values);
+                  }}
+                  layout="vertical"
+                >
+                  <Form.Item name="name" label="Name">
+                    <Input />
+                  </Form.Item>
+                  <Form.Item name={"email"} label="Email">
+                    <Input type="email" />
+                  </Form.Item>
+                  <Form.Item name="password" label="Password">
+                    <Input type="password" />
+                  </Form.Item>
+                  <Form.Item name="time" label="Pick some time">
+                    <TimePicker
+                      defaultValue={dayjs("12:08:23", "HH:mm:ss")}
+                      size="large"
+                    />
+                  </Form.Item>
+                  <Form.Item>
+                    <Button htmlType="submit" type="primary">
+                      Sign up
+                    </Button>
+                  </Form.Item>
+                </Form>
+              </Modal>
+            </Card>
+            {/*            
             <Row gutter={10}>
               <Col span={6}>
                 <Card>
@@ -137,6 +216,49 @@ function App() {
                     ]}
                   />
                 </Card>
+              </Col>
+            </Row>
+            */}
+            <Row gutter={10} style={{ marginTop: 10 }}>
+              <Col span={18}>
+                <Table
+                  dataSource={data}
+                  columns={[
+                    {
+                      dataIndex: "id",
+                      title: "ID",
+                      key: "id",
+                    },
+                    {
+                      dataIndex: "name",
+                      title: "name",
+                      key: "name",
+                    },
+                    {
+                      dataIndex: "email",
+                      title: "Email Id",
+                      key: "email",
+                    },
+                    {
+                      dataIndex: "status",
+                      title: "Status",
+                      key: "status",
+                      render: (val) =>
+                        val ? <Tag>Active</Tag> : <Tag>Not Active</Tag>,
+                    },
+                    {
+                      title: "ACtions",
+                      render: () => (
+                        <ButtonGroup>
+                          <Button>Edit</Button>
+                          <Button type="primary" danger>
+                            Delete
+                          </Button>
+                        </ButtonGroup>
+                      ),
+                    },
+                  ]}
+                />
               </Col>
             </Row>
           </Content>
